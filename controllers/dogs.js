225d8179f8f3c3418af66f15ae0c0fb4d095536c
Andrew Morrison <morrison.andrew422@gmail.com>
2023-05-24 14:25:15 -0400

    finish part 2

---
+++ controllers/dogs.js
@@ -0,0 +1,49 @@
+const { Dog } = require('../models')
+
+const create = async (req, res) => {
+  try {
+    const dog = await Dog.create(req.body)
+    console.log(dog.toJSON())
+    res.status(200).json(dog)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
+
+const index = async (req, res) => {
+  try {
+    const dogs = await Dog.findAll()
+    res.status(200).json(dogs)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
+
+const update = async (req, res) => {
+  try {
+    const dog = await Dog.findByPk(req.params.dogId)
+    dog.set(req.body)
+    await dog.save()
+
+    res.status(200).json(dog)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
+
+const deleteDog = async (req, res) => {
+  try {
+    const dog = await Dog.findByPk(req.params.dogId)
+    await dog.destroy()
+    res.status(200).json(dog)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
+
+module.exports = {
+  create,
+  index,
+  update,
+  delete: deleteDog
+}
---
+++ routes/dogs.js
@@ -0,0 +1,9 @@
+const router = require('express').Router()
+const dogsCtrl = require('../controllers/dogs.js')
+
+router.post('/', dogsCtrl.create)
+router.get('/', dogsCtrl.index)
+router.put('/:dogId', dogsCtrl.update)
+router.delete('/:dogId', dogsCtrl.delete)
+
+module.exports = router
